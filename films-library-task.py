d3cb55f85e3b6e6bf0f8e9f0537a6d099e65bd80
Kamil Prokop <zara.pug.yt@gmail.com>
2023-02-04 23:23:47 +0100

    Class Film and Serial created

---
+++ films-library-task.py
@@ -0,0 +1,23 @@
+class PlayFunction:
+    def __init__(self, views):
+        self.views = views
+
+class Film:
+    def __init__(self, title, publication_year, genre, views):
+        self.title = title
+        self.publication_year = publication_year
+        self.genre = genre
+        self.views = views
+
+class Serial:
+    def __init__(self, title, publication_year, genre, e_number, s_number, views):
+        self.title = title
+        self.publication_year = publication_year
+        self.genre = genre
+        self.e_number = e_number
+        self.s_number = s_number
+        self.views = views
+
+
+
+
